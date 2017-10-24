1bac717b434f6957ee480d7c88b760a1866e8969
theeolian <msoldmann@gmail.com>
2017-10-24 02:56:22 +0000

    Created comments routes and functionality, organized views directory

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express    = require("express"),
     bodyParser = require("body-parser"),
     mongoose   = require("mongoose"),
     Campground = require("./models/campground"),
+    Comment    = require("./models/comment"),
     seedDB     = require("./seeds");
     
 
@@ -25,7 +26,7 @@ app.get("/campgrounds", function(req, res){
             console.log(err);
         }
         else {
-            res.render("index", {campgrounds: allCampgrounds});
+            res.render("campgrounds/index", {campgrounds: allCampgrounds});
         }
     });
 });
@@ -52,7 +53,7 @@ app.post("/campgrounds", function(req, res){
 
 //NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res){
-    res.render("new.ejs");
+    res.render("campgrounds/new");
 });
 
 //SHOW - shows more info about one campground
@@ -65,11 +66,58 @@ app.get("/campgrounds/:id", function(req, res){
         else{
             console.log(foundCampground);
             // render show template with that campground
-            res.render("show", {campground: foundCampground});
+            res.render("campgrounds/show", {campground: foundCampground});
         }
     });
     
 });
+
+
+//=================
+// COMMENTS ROUTES
+//=================
+
+
+app.get("/campgrounds/:id/comments/new", function(req, res){
+    // find campground by id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        }
+        else {
+            // the value of the first "campground"
+            // is equal to the 2nd value campground which is the 
+            // campground found from the find by id above
+            res.render("comments/new", {campground: campground});
+        }
+    });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+    // lookup campground using id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        }
+        else{
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                }
+                else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect('/campgrounds/' + campground._id);
+                }
+            });
+        }
+    });
+    // create new comment
+    // connect new comment to campground
+    // redirect to campground show page
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("YelpCamp Server Running");
