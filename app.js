854d2f1391072bd8707f2ed8e650ad9ff3d8ff5f
theeolian <msoldmann@gmail.com>
2017-11-15 03:52:51 +0000

    setup env variables for dev and production

--- app.js
+++ app.js
@@ -16,8 +16,10 @@ var express    = require("express"),
 var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
-    
-mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
+
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp";
+mongoose.connect(url, {useMongoClient: true});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
